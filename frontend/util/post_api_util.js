72d6ecf579320f901cf98f9b916672726cf5c5a5
Jean <sy2803@tc.columbia.edu>
2019-12-11 15:16:50 -0500

    created post_api_util

--- frontend/util/post_api_util.js
+++ frontend/util/post_api_util.js
@@ -1,34 +1,34 @@
-// export const fetchPosts = () => (
-//     $.ajax({
-//         url: `api/posts/`
-//     })
-// );
+export const fetchPosts = () => (
+    $.ajax({
+        url: `api/posts/`
+    })
+);
 
-// export const fetchPost = postId => (
-//     $.ajax({
-//         url: `/api/posts/${postId}/`
-//     })
-// );
+export const fetchPost = postId => (
+    $.ajax({
+        url: `/api/posts/${postId}/`
+    })
+);
 
-// export const createPost = post => (
-//     $.ajax({
-//         url: `api/posts/`,
-//         method: 'POST',
-//         data: { post }
-//     })
-// );
+export const createPost = post => (
+    $.ajax({
+        url: `api/posts/`,
+        method: 'POST',
+        data: { post }
+    })
+);
 
-// export const updatePost = post => (
-//     $.ajax({
-//         url: `/api/posts/${post.id}`,
-//         method: 'patch',
-//         data: { post }
-//     })
-// );
+export const updatePost = post => (
+    $.ajax({
+        url: `/api/posts/${post.id}`,
+        method: 'patch',
+        data: { post }
+    })
+);
 
-// export const deletePost = postId => (
-//     $.ajax({
-//         url: `/api/posts/${postId}/`,
-//         method: 'DELETE'
-//     })
-// )
+export const deletePost = postId => (
+    $.ajax({
+        url: `/api/posts/${postId}/`,
+        method: 'DELETE'
+    })
+)

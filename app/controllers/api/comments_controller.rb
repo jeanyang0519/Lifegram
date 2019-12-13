bcd34fc308cda7122f72070b5651a2f6f0b5397c
Jean <sy2803@tc.columbia.edu>
2019-12-13 14:19:35 -0500

    finished comment controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,16 +1,38 @@
 class CommentsController < ApplicationController
-  def index
+  def index  
+    @comments = Comment.all 
   end
 
-  def show
+  def show 
+    @comment = Comment.find(params[:id])
+    render :show 
+  end
+  
+  def create 
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      render :show 
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+    
   end
 
-  def create
+  def destroy 
+    @comment = Comment.find(params[:id])
+    if @comment.destroy 
+      render :index
+    else 
+      render json: ['Something went wrong'], status: 404
+    end 
+
   end
 
-  def destroy
-  end
+  private 
 
   def comment_params
+    params.require(:comment).permit(:comment_body, :post_id, :user_id)
   end
 end
